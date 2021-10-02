871d25f98c98583d2532b8b59fa3b1507fe09c27
zjrb <61639397+zjrb@users.noreply.github.com>
2021-10-01 20:55:23 -0600

    Updated Main.cpp with user input

--- main.cpp
+++ main.cpp
@@ -3,14 +3,31 @@
 int main()
 {
     RomanNumerals test;
-    try{
-        std::cout << test.convertNumeral("MMMCMXCVII");
-    }
-    catch(char const* error)
-    {
-        std::cout << error;
-    };
-    std::cout << std::endl << test.convertDecimal(3997);
+    std::string ans = "";
+    while (ans != "Q") {
+        std::cout << "To Convert a number to Roman Numeral please type 'C' " << std::endl
+                  << "To convert a Roman Numeral to a number please type 'X' " << std::endl
+                  << "To exit please type 'Q'" << std::endl;
+        std::cin >> ans;
+        if (ans == "C")
+        {
+            int num = 0;
+            std::cout << "Please enter a number  NUMBER CANNOT BE GREATER THAN 3999 or a NEGATIVE NUMBER \n";
+            std::cin >> num;
+            std::cout << "Answer is : " << test.convertDecimal(num) << std::endl;
+        }
+        else if (ans == "X")
+        {
+            std::string numeral;
+            std::cout << "Please enter a valid Roman Numeral \n";
+            std::cin >> numeral;
+            std::cout << "Answer is : " << test.convertNumeral(numeral) << std::endl;
+        }
+        else if (ans != "C" && ans != "X" && ans != "Q")
+        {
+                std::cout << "INPUT IS INVALID PLEASE ENTER A VALID INPUT" << std::endl;
+        }
+        }
 
     return 0;
 }
